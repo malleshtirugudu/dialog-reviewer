fbad533b3c7ad65e5e5013d7f909a05e1a822984
Mallesh <mallesh@Malleshs-MacBook-Pro.local>
2020-03-12 01:18:45 +0530

    Updated table and content

--- src/components/usertable/usertable.component.ts
+++ src/components/usertable/usertable.component.ts
@@ -16,96 +16,100 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 
 export class UsertableComponent {
   dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+  columnsToDisplay = ['UserId', 'Timestamp', 'Conversation', 'Rating', 'FeedBack', 'ReviewStatus'];
   expandedElement: PeriodicElement | null;
 }
 
 export interface PeriodicElement {
-  name: string;
-  userId: number;
-  weight: number;
-  symbol: string;
+  Timestamp: string;
+  UserId: number;
+  Conversation: string;
+  Rating: string;
+  FeedBack: string,
+  ReviewStatus: string,
   description: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {
-    userId: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+    UserId: 1,
+    Timestamp: '20-02-2020 00:24:59',
+    Conversation: '12 Total Converation',
+    Rating: '1 Star',
+    FeedBack: 'Good',
+    ReviewStatus: 'Partially Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    symbol: 'He',
-    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
+    UserId: 2,
+    Timestamp: '20-02-2020 03:10:10',
+    Conversation: '4 Total Converation',
+    Rating: '2 Star',
+    FeedBack: 'Ok',
+    ReviewStatus: 'Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    symbol: 'Li',
-    description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`
+    UserId: 3,
+    Timestamp: '20-02-2020 05:04:13',
+    Conversation: '10 Total Converation',
+    Rating: '5 Star',
+    FeedBack: 'Not Bad',
+    ReviewStatus: 'Not Started',
+    description: `Conversation Log comes here`
   }, {
-    userId: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be',
-    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`
+    UserId: 4,
+    Timestamp: '20-02-2020 06:00:05',
+    Conversation: '13 Total Converation',
+    Rating: '3 Star',
+    FeedBack: 'Bad',
+    ReviewStatus: 'Not Started',
+    description: `Conversation Log comes here`
   }, {
-    userId: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B',
-    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`
+    UserId: 5,
+    Timestamp: '20-02-2020 07:24:59',
+    Conversation: '14 Total Converation',
+    Rating: '1 Star',
+    FeedBack: 'Good',
+    ReviewStatus: 'Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C',
-    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`
+    UserId: 6,
+    Timestamp: '20-02-2020 08:10:10',
+    Conversation: '12 Total Converation',
+    Rating: '5 Star',
+    FeedBack: 'Not Bad',
+    ReviewStatus: 'Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N',
-    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
+    UserId: 7,
+    Timestamp: '20-02-2020 08:24:13',
+    Conversation: '9 Total Converation',
+    Rating: '1 Star',
+    FeedBack: 'Good',
+    ReviewStatus: 'Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O',
-    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`
+    UserId: 8,
+    Timestamp: '20-02-2020 09:00:05',
+    Conversation: '11 Total Converation',
+    Rating: '3 Star',
+    FeedBack: 'Good',
+    ReviewStatus: 'Partially Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F',
-    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`
+    UserId: 9,
+    Timestamp: '9,20-02-2020 12:24:59,',
+    Conversation: '4 Total Converation',
+    Rating: '4 Star',
+    FeedBack: 'Not Bad',
+    ReviewStatus: 'Completed',
+    description: `Conversation Log comes here`
   }, {
-    userId: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne',
-    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`
+    UserId: 10,
+    Timestamp: '20-02-2020 09:00:05',
+    Conversation: '8 Total Converation',
+    Rating: '5 Star',
+    FeedBack: 'Bad',
+    ReviewStatus: 'Partially Completed',
+    description: `Conversation Log comes here`
   },
 ];
